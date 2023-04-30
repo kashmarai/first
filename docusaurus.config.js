f1aa205a899ff5d1b4f3de4bb2476c75e823086c
Rajat Venkatesh <vrajat@users.noreply.github.com>
2023-04-30 19:31:03 +0530

    Fix footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,7 +70,7 @@ const config = {
         style: 'dark',
         links: [
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Kromlogs`,
       },
       prism: {
         theme: lightCodeTheme,
